b34d9b895295bedd150b48ad47d9130c5117e6f7
MaximFet <100441347+MaximFet@users.noreply.github.com>
2022-02-27 22:22:58 +0300

    добавил первый скрипт

---
+++ lesson1/index.js
@@ -0,0 +1,8 @@
+console.log("Hello Jvascript");
+const myPlanet ='EARTH';
+console.log (myPlanet);
+let currentTime='22;57Time';
+currentTime = 23.04;
+console.log (currentTime);
+currentTime = 23.15;
+console.log (currentTime);
